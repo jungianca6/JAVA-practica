a9bf1a4750f387e260c89fe5b22e07a34a97047b
Giancarlo Vega Marin <131577830+jungianca6@users.noreply.github.com>
2023-08-18 01:05:31 -0600

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,25 +1,28 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
 import java.util.Random;
 
 
 public class Main {
     public static void main(String args[]) {
-        int n = 10;
-        int vec[] = new int[n];
-        int i;
-        for (i = 0; i < n; i++) {
-            Random random = new Random();
-            vec[i] = random.nextInt(10);
-
-        }
-        System.out.print("[");
-        for (i = 0; i < n; i++) {
-            if (i==n-1) {
-                System.out.print(vec[i]);
-            }
-            else {
-                System.out.print(vec[i] +",");
+        int i = 10;
+        int j = 15;
+        int [][] mat = new int[i][j];
+        int x;
+        int y;
+        for (x = 0; x < i; x++) {
+            for (y=0;y<j;y++) {
+                Random random = new Random();
+                mat[x][y] = random.nextInt(10);
             }
         }
-        System.out.print("]");
+        //System.out.print("[");
+        for (x = 0; x < i; x++) {
+            for (y=0;y<j;y++){
+                System.out.print(mat[x][y]+" ");
+            }
+            System.out.println("");
+        }
+        //System.out.println("]");
     }
 }
